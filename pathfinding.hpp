d7c0dbbc57b5ac22285acdbbdcbc9d0eb6772682
morrrrrrrr <moritz.weissenboeck@gmx.at>
2023-11-11 08:59:38 -0800

    Began working on pathfinding.hpp Added tests folder Added tests/test.cpp file

--- pathfinding.hpp
+++ pathfinding.hpp
@@ -2,3 +2,26 @@
  * File: pathfinding.hpp
  * Desc: 
  */
+
+#define PATHFINDING_HPP
+#ifndef PATHFINDING_HPP
+
+#include <vector>
+
+namespace pathfinding {
+    struct Node {
+        int x, y;
+        
+        // Constructors
+        Node() : x(0), y(0) { }
+        Node(int x, int y) : x(x), y(y) { }
+    };
+
+    using Grid = std::vector<std::vector<int>>;
+
+    static int operator[](const Grid& grid, const Node& node) {
+        return grid[node.x][node.y];
+    }
+} // namespace pathfinding
+
+#endif
---
+++ tests/test.cpp
@@ -0,0 +1,4 @@
+
+#include "pathfinding.hpp"
+
+
