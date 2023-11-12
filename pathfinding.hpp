a9a4bf73952aaddfedd52a61e00a07bc72e6f474
morrrrrrrr <moritz.weissenboeck@gmx.at>
2023-11-11 18:52:06 -0800

    Added Functions to get the Grid Value and Size Added Test function in tests/test.cpp

--- pathfinding.hpp
+++ pathfinding.hpp
@@ -3,25 +3,84 @@
  * Desc: 
  */
 
-#define PATHFINDING_HPP
 #ifndef PATHFINDING_HPP
+#define PATHFINDING_HPP
 
 #include <vector>
+#include <string>
+#include <stdexcept>
 
 namespace pathfinding {
+    // Structure representing a 2D point.
     struct Node {
         int x, y;
         
         // Constructors
-        Node() : x(0), y(0) { }
-        Node(int x, int y) : x(x), y(y) { }
+        inline Node() : x(0), y(0) { }
+        inline Node(int x, int y) : x(x), y(y) { }
     };
 
-    using Grid = std::vector<std::vector<int>>;
+    // Template for a 2D grid.
+    template<typename T>
+    using Grid = std::vector<std::vector<T>>;
 
-    static int operator[](const Grid& grid, const Node& node) {
-        return grid[node.x][node.y];
+    // Access a mutable value in the grid based on a Node.
+    template<typename T>
+    inline T& getGridValue(Grid<T>& grid, const Node& node) {
+        try { // Handle out-of-range access gracefully.
+            return grid.at(node.y).at(node.x); 
+        }
+        catch (std::out_of_range& err) {
+            throw std::out_of_range("Grid access error: " + std::string(err.what()));
+        }
     }
+    // Access a mutable value in the grid based on x and y coordinates.
+    template<typename T>
+    inline T& getGridValue(Grid<T>& grid, const int& x, const int& y) {
+        try { // Handle out-of-range access gracefully.
+            return grid.at(y).at(x); 
+        }
+        catch (std::out_of_range& err) {
+            throw std::out_of_range("Grid access error: " + std::string(err.what()));
+        }
+    }
+
+    // Access an immutable value in the grid based on a Node.
+    template<typename T>
+    inline const T& getGridValue(const Grid<T>& grid, const Node& node) {
+        try { // Handle out-of-range access gracefully.
+            return grid.at(node.y).at(node.x); 
+        }
+        catch (std::out_of_range& err) {
+            throw std::out_of_range("Grid access error: " + std::string(err.what()));
+        }
+    }
+    // Access an immutable value in the grid based on x and y coordinates.
+    template<typename T>
+    inline const T& getGridValue(const Grid<T>& grid, const int& x, const int& y) {
+        try { // Handle out-of-range access gracefully.
+            return grid.at(y).at(x); 
+        }
+        catch (std::out_of_range& err) {
+            throw std::out_of_range("Grid access error: " + std::string(err.what()));
+        }
+    }
+
+    // Get the size of the grid. returns the size as a Node-struct
+    // (Node.x => grid size x) 
+    // (Node.y => grid size y)
+    template<typename T>
+    inline const Node getGridSize(const Grid<T>& grid) {
+        return Node(grid[0].size(), grid.size());
+    }
+
+    // A template class for the a*-pathfinding algorithms.
+    template<typename T>
+    class Pathfinder {
+        Grid<T> grid;
+    public:
+        Pathfinder(const Grid<T>& grid) : grid(grid) { };
+    };
 } // namespace pathfinding
 
 #endif
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,4 +1,43 @@
 
-#include "pathfinding.hpp"
+// include pathfinding.hpp from folder below (this may change in a use-case)
+#include "../pathfinding.hpp"
 
+#include <vector>
+#include <iostream>
+#include <string>
 
+void testGetFunctions(const pathfinding::Grid<int>& grid) {
+    namespace pf = pathfinding;
+
+    std::cout << "Test of the Get-Functions:\n";
+    std::cout << "\tPrint the whole Grid:\n";
+    // loop through the grid
+    for (int y = 0; y < pf::getGridSize(grid).y; y++) {
+        for (int x = 0; x < pf::getGridSize(grid).x; x++) { 
+            std::cout << std::to_string(pf::getGridValue(grid, x, y));
+        }
+        std::cout << "\n";
+    }
+
+    std::cout << "\tPrint grid size:\n";
+    // in node.x is the grid X size, int node.y the grid Y size
+    const pf::Node size = pf::getGridSize(grid);
+    std::cout << "Size X: " << size.x << ", Size Y: " << size.y << "\n";
+
+    // throws exception correctly
+    // const int i = pf::getGridValue(grid, -1, -1);
+}
+
+int main(int argc, char** argv) {
+    const pathfinding::Grid<int> grid {
+        { 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0 },
+        { 0, 1, 1, 1, 0 },
+        { 0, 0, 0, 1, 0 },
+        { 0, 0, 0, 0, 0 }
+    };
+
+    testGetFunctions(grid);
+
+    return 0;
+}
